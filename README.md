c96b0875f56b6e8cdea9fdc9c7876c88c7e3ac4a
Eka Putra <ekaputra@balitechy.com>
2016-02-04 21:16:23 +0800

    Added more operations.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # python-nowdb
 A simple python wrapper for NowDB API http://nowdb.net.
 
-Under development...
+Available operations:
+
+* insert
+* select_all
+* select_id
+* select_where
--- nowdb/api.py
+++ nowdb/api.py
@@ -44,24 +44,85 @@ class NowDBAPI(object):
             raise NowDBException('Request timeout')
         except exceptions.HTTPError as e:
             raise NowDBException('Invalid HTTP response')
-        else:
+
+        # In case server return invalid JSON, return plain text response.
+        try:
             return resp.json()
+        except ValueError as e:
+            return resp.text
+
+    def insert(self, **kwargs):
+        """
+        @operation: insert
+        @params:
+            kwargs: data to insert
+        """
+        return self.__post('insert', kwargs)
 
     def select_all(self, **kwargs):
         """
-        Operation: select_all
+        @operation: select_all
+        @params:
+            kwargs: limit, offset, order, mixin
         """
         return self.__post('select_all', kwargs)
 
+    def select_id(self, id):
+        """
+        @operation: select_id
+        @params:
+            id: String, the object id.
+        """
+        return self.__post('select_id', {'id': id})
+
+    def select_where(self, **kwargs):
+        """
+        @operation: select_where
+        @params:
+            kwargs: attribute 1, attribute 2, offset, order, mixin
+        @example: api.select_where(title='hello', offset=0, limit=10, order='title')
+        """
+        return self.__post('select_where', kwargs)
+
 
 if __name__ == '__main__':
-    api = NowDBAPI(token='555002ab88d909e8324aaea01',
+    api = NowDBAPI(token='55002ab88d909e8324aaea01',
                    project='python_nowdb',
-                   app_id='556af6e3c1f6d04391f004740')
+                   app_id='56af6e3c1f6d04391f004740')
     api.set_collection('sample')
 
+    # Test - insert
+    print 'Testing: insert =============='
     try:
-        result = api.select_all(limit=5)
+        result = api.insert(title="Hello NowDB")
+    except NowDBException as e:
+        print e.message
+    else:
+        print result
+    obj_id = result[0]['id']
+
+    # Test - select_all
+    print '\nTesting: select_all =============='
+    try:
+        result = api.select_all(limit=1)
+    except NowDBException as e:
+        print e.message
+    else:
+        print result
+
+    # Test - select_id
+    print '\nTesting: select_id =============='
+    try:
+        result = api.select_id(obj_id)
+    except NowDBException as e:
+        print e.message
+    else:
+        print result
+
+    # Test - select_where
+    print '\nTesting: select_where =============='
+    try:
+        result = api.select_where(title='Hello NowDB')
     except NowDBException as e:
         print e.message
     else:
